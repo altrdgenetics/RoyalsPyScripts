de882fae6b5cd03ecbe69665c5e717d3d58c46c9
altrdgenetics <tofudeliveryae86@hotmail.com>
2016-12-11 00:25:49 -0500

    Added some comments

--- logReader.py
+++ logReader.py
@@ -1,6 +1,7 @@
 import os
 import time
 
+# Request File & Search Parameters
 filePath = str(input("Enter Full File Path: "))
 defaultSearchTermTCP = str("Blocked incoming TCP connection request")
 defaultSearchTermUDP = str("Blocked incoming UDP connection request")
@@ -10,25 +11,34 @@ searchTerm = str(input("(separate terms with space): "))'''
 exists = os.path.isfile(filePath)
 
 if exists:
+    # Initialize Variables
     startDate = str("")
     startTime = str("")
     endDate = str("")
     endTime = str("")
     firstLine = True
     '''searchTermSplit = searchTerm.upper().split()'''
+
+    # Open Log File & Parse it
     logfile = open(os.path.abspath(filePath), 'r')
     result_file = open('results_' + str(time.time()).replace('.', '') + '.txt', 'w+')
     with logfile as f:
         for line in f:
             if (line.__contains__(defaultSearchTermUDP)) or (line.__contains__(defaultSearchTermTCP)):
                 '''if any(x in line.upper() for x in searchTermSplit):'''
+
+                # Parse Line Information
                 date = str(line[0:6]).replace("  ", " ").strip()
                 time = str(line[7:15]).strip()
                 proto = str(line[94:97]).strip()
                 ip = "{:<15}".format(str(line.split("from", 1)[1].split(":")[0]).strip())
                 port = str(line.rsplit(":", 1)[1]).strip()
+
+                # Write Line Information
                 result_file.write("DATE= " + date + "   TIME= " + time + "    PROTO= " + proto + "    IP= " + ip
                                   + "    PORT= " + port + "\n")
+
+                # Results File Naming Convention
                 if firstLine:
                     startDate = date.replace(" ", "_")
                     startTime = time
@@ -37,6 +47,8 @@ if exists:
                 endTime = time
     logfile.close()
     result_file.close()
+
+    # Rename Results File
     newName = str("results_" + startDate + "_" + startTime + "_" + endDate + "_" + endTime + ".txt")
     os.rename(result_file.name, newName.replace(":", "_"))
 else:
